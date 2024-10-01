9871b8c6b6d3c1991737c592e173d43e8b1fa616
Ajith <39941665+ajithAI@users.noreply.github.com>
2024-10-01 11:03:43 +0530

    Update docker_run_llama_inference.sh

--- docker_run_llama_inference.sh
+++ docker_run_llama_inference.sh
@@ -6,7 +6,7 @@ FILE=${FOLDER}/LLaMA3.1_70B_TRT_Batch_${1}_Input_${2}_Output_${3}_${6}
 turbostat -i 1 > ${FILE}_TURBOSTAT.txt &
 nvidia-smi dmon -s pucvt > ${FILE}_NVIDIASMI.txt &
 sleep 2s
-docker exec TensorRT_LLM_8xGPU_CUDA_12.6.0 bash /home/user/LLaMA3.1_70B_Inference/run_llama_inference.sh ${1} ${2} ${3} ${4} ${5} ${FILE}
+docker exec TensorRT_LLM_8xGPU_CUDA_12.6.0 bash /home/user/LLaMA3.1_Inference/run_llama_inference.sh ${1} ${2} ${3} ${4} ${5} ${FILE}
 sleep 2s
 pkill -9 turbostat
 pkill -9 nvidia-smi
